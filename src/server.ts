4690134d98b08603f24f4b0a33cfcbfb86d6164b
Ahmed Fareed <ahmed@techchaps.com>
2024-11-25 01:09:36 +0000

    make the app available for public access

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app from './app';
 
-const PORT = process.env.PORT || 3000;
+const PORT = parseInt(process.env.PORT || '3000', 10);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
